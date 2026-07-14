0e454f8266599a62d6e72da4282af07ca7846918
“sheetung” <755855262@qq.com>
2026-07-14 17:14:54 +0800

    Fix frontend notification styles

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,6 +5,7 @@
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import 'element-plus/es/components/message/style/css'
 
 import App from './App.vue'
 import router from './router'
